e38e7268383dabcc1dfbbb659c430850ee9ec1e9
Banhhmii <tommyngo156@gmail.com>
2025-01-01 01:19:45 -0500

    added help and exit command

--- main.go
+++ main.go
@@ -1,11 +1,7 @@
 package main
 
-import (
-	"fmt"
-)
 
 func main() {
-	fmt.Println("Hello, World!")
+	//fmt.Println("Hello, World!")
+	startRepl()
 }
-
-
--- repl.go
+++ repl.go
@@ -1,6 +1,13 @@
 package main
 
-import ("strings")
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+)
+
 
 func cleanInput(text string) []string {
 	lowered := strings.ToLower(text)
@@ -8,4 +15,55 @@ func cleanInput(text string) []string {
 	splitText := strings.Fields(trimmed)
 
 	return splitText
+}
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for  {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+
+		words := cleanInput((scanner.Text()))
+		if len(words) == 0 {
+			continue
+		}
+
+		commandName := words[0]
+
+		command, exists := getCommands()[commandName]
+
+		if exists {
+			err := command.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+			continue
+		}else {
+			fmt.Println("Unknown command")
+			continue
+		}
+
+
+	}
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name: "help",
+			description: "Displays a help message",
+			callback: commandHelp,
+		},
+	}
 }
